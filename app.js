dd84b9ddd8a75b4b69c3e0999d00a679e2e54b08
zjria <zakaria.com.bd@gmail.com>
2023-03-18 11:15:34 +0600

    button add drop down

--- app.js
+++ app.js
@@ -31,3 +31,26 @@ var swiper = new Swiper(".mySwiper", {
         },
     },
 });
+
+// drop down menu
+const dropdown = document.querySelector('.drop-down');
+const list = document.querySelector('.list');
+
+const selected = document.querySelector('.selected');
+
+const selectedImg = document.querySelector('.selectedImg');
+console.log(selectedImg);
+
+
+dropdown.addEventListener('click', () => {
+    list.classList.toggle('show');
+})
+
+list.addEventListener('click', (e) => {
+    const img = e.target.querySelector('img');
+    const text = e.target.querySelector('.text');
+
+    selectedImg.src = img.src;
+    selected.innerHTML = text.innerHTML;
+
+})
